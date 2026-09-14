9c9cbd5e94696a2efdc4ff170d69bcb4b8f31d58
paulopavlak2014 <paulopavlak2014@gmail.com>
2026-09-14 09:37:04 -0300

    feat: GamesDayController com auth + AuthModule no module

--- apps/api/src/games-day/games-day.module.ts
+++ apps/api/src/games-day/games-day.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 import { GamesDayService } from './games-day.service';
+import { GamesDayController } from './games-day.controller';
 
 @Module({
-  imports: [ScheduleModule, PrismaModule],
+  imports: [ScheduleModule, PrismaModule, AuthModule],
+  controllers: [GamesDayController],
   providers: [GamesDayService],
   exports: [GamesDayService],
 })
